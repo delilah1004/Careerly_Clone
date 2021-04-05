5e5e52230745ab556830d1c99ba7f02a80e949e8
Delilah1004 <pskdy8414@naver.com>
2021-04-05 15:59:19 +0900

    feat: 알림페이지 추가

--- pages/Notificate.jsx
+++ pages/Notificate.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, Dimensions } from 'react-native';
 import { Container } from 'native-base';
+
 import HeaderTitle from '../components/header/HeaderTitle';
+
+import bell from '../assets/bell.jpg';
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function Notification() {
   return (
     <Container>
       <HeaderTitle title={'알림'} />
       <View style={styles.container}>
-        <Text>careerly notification!</Text>
+        <Image style={styles.bell} source={bell} />
+        <Text style={styles.text}>아직 받아볼 알림이 없어요.</Text>
       </View>
     </Container>
   );
@@ -19,5 +25,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  bell: {
+    width: windowWidth / 3,
+    height: windowWidth / 3,
+  },
+  text: {
+    fontSize: 16,
+    color: '#777',
   },
 });
