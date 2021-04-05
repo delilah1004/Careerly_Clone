da7d1a3ebe2ed9cb648f59cd5a7d2d935a677868
Delilah1004 <pskdy8414@naver.com>
2021-04-06 02:30:29 +0900

    회원가입시 이메일 체크 구현

--- pages/begin/SignUp.jsx
+++ pages/begin/SignUp.jsx
@@ -1,42 +1,39 @@
 import React, { useState } from 'react';
-
-import { StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import { Container, Text, Form, View } from 'native-base';
 
 import InputItem from '../../components/InputItem';
-import ButtonItem from '../../components/ButtonItem';
 import TextButton from '../../components/TextButton';
 
+import { emailCheck } from '../../config/APIFunctions';
+
 export default function SignUpPage({ navigation }) {
-  const [nickName, setNickName] = useState('');
-  const [nickNameError, setNickNameError] = useState('');
-
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-
   const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
 
-  const doSignUp = () => {
-    if (nickName == '') {
-      setNickNameError('이름을 입력해주세요');
-      return false;
+  const check = () => {
+    emailCheck(name, email, password, navigation);
+  };
+
+  let showButton = () => {
+    if (name == '' || email == '' || password == '') {
+      return (
+        <TouchableOpacity disabled style={[styles.button, styles.disabled]}>
+          <Text style={styles.text}>이메일로 시작하기</Text>
+        </TouchableOpacity>
+      );
     } else {
-      setNickNameError('');
+      return (
+        <TouchableOpacity
+          style={[styles.button, styles.active]}
+          onPress={check}
+        >
+          <Text style={styles.text}>이메일로 시작하기</Text>
+        </TouchableOpacity>
+      );
     }
-    if (email == '') {
-      setEmailError('이메일을 입력해주세요');
-      return false;
-    } else {
-      setEmailError('');
-    }
-    if (password == '') {
-      setPasswordError('비밀번호를 입력해주세요');
-      return false;
-    } else {
-      setPasswordError('');
-    }
-    registration(nickName, email, password);
   };
 
   return (
@@ -46,43 +43,31 @@ export default function SignUpPage({ navigation }) {
           <Text style={styles.titleText}>가입하고 커리어리를</Text>
           <Text style={styles.titleText}>무료로 이용하세요</Text>
         </View>
-
         <Form style={styles.form}>
           <InputItem
             title={'이름'}
             hint={'이름 입력'}
-            type={'nickName'}
-            value={nickName}
-            error={nickNameError}
-            setFunc={setNickName}
+            type={'name'}
+            setFunc={setName}
           />
           <InputItem
             title={'이메일'}
             hint={'이메일 입력'}
             type={'email'}
-            value={email}
-            error={emailError}
             setFunc={setEmail}
           />
           <InputItem
             title={'비밀번호'}
             hint={'비밀번호 입력'}
             type={'password'}
-            value={password}
-            error={passwordError}
             setFunc={setPassword}
           />
         </Form>
-
         <Text style={styles.terms}>
           {`'회원가입'을 누름으로써\n커리어리의 이용약관과 개인정보처리 방침에 동의합니다`}
         </Text>
 
-        <ButtonItem
-          title="이메일로 시작하기"
-          navigation={navigation}
-          page={'TabNavigator'}
-        />
+        {showButton()}
 
         <View style={styles.textButtonContainer}>
           <TextButton
@@ -112,6 +97,7 @@ export default function SignUpPage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: getStatusBarHeight(),
     width: '90%',
     justifyContent: 'center',
     alignSelf: 'center',
@@ -135,6 +121,24 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     fontSize: 12,
     color: 'grey',
+  },
+  button: {
+    backgroundColor: '#ed6653',
+    width: '100%',
+    height: 50,
+    borderRadius: 5,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  active: {
+    opacity: 1,
+  },
+  disabled: {
+    opacity: 0.5,
+  },
+  text: {
+    color: 'white',
   },
   textButtonContainer: {
     flexDirection: 'row',
