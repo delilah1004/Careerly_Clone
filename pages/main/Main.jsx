e34f71d1cfdfe1377c146aee3b948c06b894e170
Delilah1004 <pskdy8414@naver.com>
2021-04-07 02:25:39 +0900

    fix: 메인 페이지 디자인 수정

--- pages/main/Main.jsx
+++ pages/main/Main.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
-import { View, Text } from 'native-base';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import {
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import { View, Text, Grid, Col, Container } from 'native-base';
 
 import { FontAwesome } from '@expo/vector-icons';
 
 import VoteCard from '../../components/vote/VoteCard';
 import MainCard from '../../components/main/MainCard';
+
+const WindowWidth = Dimensions.get('window').width;
+const ThumbSize = WindowWidth * 0.12;
 
 export default function Main({ navigation }) {
   useEffect(() => {
@@ -15,79 +24,86 @@ export default function Main({ navigation }) {
   }, []);
 
   return (
-    <ScrollView
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'rgba(52, 52, 52, 0.1)',
-      }}
-    >
+    <Container style={styles.container}>
       <ScrollView
-        style={{ marginTop: 50, maxHeight: 210, alignSelf: 'center' }}
-        horizontal={true}
-      >
-        <VoteCard navigation={navigation} />
-        <VoteCard navigation={navigation} />
-        <VoteCard navigation={navigation} />
-      </ScrollView>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('PostCreate');
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          backgroundColor: 'rgba(52, 52, 52, 0.1)',
         }}
       >
-        <View
-          style={{
-            marginTop: 30,
-            marginBottom: 20,
-            width: 420,
-            backgroundColor: '#FFF',
-            height: 80,
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            borderRadius: 20,
+        {/* 투표 */}
+        <ScrollView
+          style={{ marginVertical: 20 }}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.voteList}>
+            <VoteCard navigation={navigation} vote={''} />
+            <VoteCard navigation={navigation} vote={''} />
+            <VoteCard navigation={navigation} vote={''} />
+          </View>
+        </ScrollView>
+
+        {/* 글쓰기 */}
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('PostCreate');
           }}
         >
-          <FontAwesome
-            style={{ marginLeft: 25 }}
-            name="user-circle-o"
-            size={35}
-            color="black"
-          />
-          <Text
-            style={{
-              marginLeft: 80,
-              padding: 15,
-              position: 'absolute',
-              textAlign: 'center',
-              backgroundColor: 'rgba(52, 52, 52, 0.05)',
-              width: 280,
-              color: 'gray',
-            }}
-          >
-            함께 나누고 싶은 생각이 있나요?
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <ScrollView>
+          <Grid style={styles.createBox}>
+            <Col size={2}>
+              <FontAwesome
+                style={{ alignSelf: 'center' }}
+                name="user-circle-o"
+                size={ThumbSize}
+                color="#C7C7C7"
+              />
+            </Col>
+            <Col size={8}>
+              <Text
+                style={{
+                  fontSize: 15,
+                  backgroundColor: '#F7F7F7',
+                  color: '#AAA',
+                  padding: 18,
+                  borderRadius: 5,
+                }}
+              >
+                함께 나누고 싶은 생각이 있으신가요?
+              </Text>
+            </Col>
+          </Grid>
+        </TouchableOpacity>
+
+        {/* 글 목록 */}
         <MainCard navigation={navigation} />
         <MainCard navigation={navigation} />
         <MainCard navigation={navigation} />
         <MainCard navigation={navigation} />
         <MainCard navigation={navigation} />
       </ScrollView>
-    </ScrollView>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: getStatusBarHeight(),
   },
-  CardItem: {
-    width: 300,
-    height: 200,
-    borderRadius: 10,
+  voteList: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingLeft: 15,
+  },
+  createBox: {
+    backgroundColor: '#FFF',
+    marginVertical: 10,
+    paddingVertical: 15,
+    paddingLeft: 5,
+    paddingRight: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
