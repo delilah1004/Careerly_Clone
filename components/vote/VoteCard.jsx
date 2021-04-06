6f40ad9f0a393ce52138b62d308b6dc23159a722
Delilah1004 <pskdy8414@naver.com>
2021-04-07 02:24:31 +0900

    fix: 투표 카드 디자인 수정

--- components/vote/VoteCard.jsx
+++ components/vote/VoteCard.jsx
@@ -1,69 +1,104 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, Thumbnail } from 'native-base';
 
-import { Col, Row, Grid } from 'react-native-easy-grid';
-import RNUrlPreview from 'react-native-url-preview';
-import {
-  Container,
-  Header,
-  Content,
-  Left,
-  Icon,
-  Right,
-  Text,
-  Button,
-  Card,
-  CardItem,
-  Thumbnail,
-} from 'native-base';
-
-import {
-  Foundation,
-  Ionicons,
-  Fontisto,
-  FontAwesome,
-} from '@expo/vector-icons';
 const im = require('../../assets/icon.png');
 
-export default function VoteCard({ navigation }) {
+const WindowWidth = Dimensions.get('window').width;
+const CardWidth = WindowWidth * 0.86;
+const ThumbSize = WindowWidth * 0.12;
+
+export default function VoteCard({ navigation, vote }) {
   return (
-    <TouchableOpacity>
-      <Card style={{ width: 280, height: 200, borderRadius: 10, elevation: 3 }}>
-        <CardItem header>
-          <Text
-            style={{
-              borderRadius: 5,
-              padding: 5,
-              textAlign: 'center',
-              color: 'blue',
-              backgroundColor: '#D1ECFF',
-            }}
-          >
-            투표
-          </Text>
-          <Text style={{ marginLeft: 150, fontSize: 14, color: 'gray' }}>
-            ~4월 7일까지
-          </Text>
-        </CardItem>
-        <Text style={{ marginLeft: 20 }}>세상에 좋은 팀장이 있다? 없다?</Text>
-        <CardItem footer>
-          <View style={{ marginTop: 30, flexDirection: 'row' }}>
-            <Thumbnail small source={im} />
-            <Text style={{ alignSelf: 'center', marginLeft: 10 }}>홍길동</Text>
-            <Text
-              style={{
-                alignSelf: 'center',
-                position: 'absolute',
-                marginLeft: 50,
-                fontSize: 15,
-                paddingTop: 50,
-              }}
-            >
-              HR
-            </Text>
-          </View>
-        </CardItem>
-      </Card>
+    <TouchableOpacity style={styles.card}>
+      {/* Header */}
+      <View style={styles.itemHeader}>
+        {/* 투표 표시 */}
+        <Text style={styles.badge}>투표</Text>
+
+        {/* 날짜 */}
+        <Text style={styles.deadLine}>~4월 7일까지</Text>
+      </View>
+
+      {/* Body */}
+      <View style={styles.itemBody}>
+        {/* 투표 제목 */}
+        <Text ellipsizeMode={'tail'} numberOfLines={2} style={styles.voteTitle}>
+          세상에 좋은 팀장이 있다? 없다?
+        </Text>
+      </View>
+
+      {/* Footer */}
+      {/* 투표 작성자 정보 */}
+      <View style={styles.itemFooter}>
+        {/* 투표 작성자 이미지 */}
+        <Thumbnail style={styles.thumbnail} source={im} />
+
+        <View style={styles.infoBox}>
+          {/* 투표 작성자 이름 */}
+          <Text style={styles.authorName}>홍길동</Text>
+
+          {/* 투표 작성자 직함 */}
+          <Text style={styles.authorRole}>HR</Text>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#FFF',
+    width: CardWidth,
+    padding: 20,
+    marginRight: 15,
+    borderRadius: 10,
+  },
+
+  itemHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  badge: {
+    backgroundColor: '#E6F2FD',
+    color: '#3797E8',
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+  deadLine: {
+    color: '#AAA',
+    fontSize: 13,
+  },
+
+  itemBody: {
+    height: CardWidth * 0.2,
+    marginVertical: 10,
+  },
+  voteTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  itemFooter: {
+    flexDirection: 'row',
+  },
+  thumbnail: {
+    width: ThumbSize,
+    height: ThumbSize,
+  },
+  infoBox: {
+    marginLeft: 10,
+  },
+  authorName: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  authorRole: {
+    color: '#999',
+    fontSize: 15,
+  },
+});
