3273f778d9aa98291fb8aa52d4b7f9668d60b0c8
NOhhwasung <sinical909@naver.com>
2021-04-07 20:17:23 +0900

    아이콘, 스플래시, 로딩 추가

--- pages/Loading.jsx
+++ pages/Loading.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+
+const backImage = require('../assets/load.gif');
+
+const WindowWidth = Dimensions.get('window').width;
+const ThumbSize = WindowWidth * 0.12;
 
 export default function Loading() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Loading...</Text>
+      <ImageBackground
+        source={backImage}
+        style={styles.backImage}
+      ></ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    //앱의 배경 색
-    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fdc453',
+    backgroundColor: '#FDFBF7',
+  },
+  backImage: {
+    width: WindowWidth,
+    height: WindowWidth * 2,
+    justifyContent: 'center',
+    alignSelf: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: '700',
+    textAlign: 'center',
   },
 });
