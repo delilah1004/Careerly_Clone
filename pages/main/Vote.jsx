cf095e955723b220a1f38bcb9454f4b36b635610
Delilah1004 <pskdy8414@naver.com>
2021-04-08 10:44:12 +0900

    fix: Vote 스켈레톤 코드 작성

---
+++ pages/main/Vote.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
+import { Container } from 'native-base';
+
+export default function Vote() {
+  return <Container></Container>;
+}
